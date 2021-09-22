a175e611d45749a545fb5bf758778ceb7136d9cb
beccakosey22 <820512@mystma.org>
2021-09-22 08:46:41 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-#WRITE YOUR CODE IN THIS FILE
+#create a function called product3Plus1() that returns the product of thre given variables (x, y, z) plus 1
+def product3Plus1(x, y, z):
+    return (x * y *z) + 1
+
+print(product3Plus1(3, 4, 5))
